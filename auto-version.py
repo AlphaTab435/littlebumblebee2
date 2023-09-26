eca07bb786e33d05e26bb346af7eff7866693473
AlphaTab435 <officeusesep@gmail.com>
2023-09-26 17:41:40 +0500

    v2

--- auto-version.py
+++ auto-version.py
@@ -1,6 +1,6 @@
 imageName="        image: muhammadtabish/bankist-app"
 
-with open('kustomize/deployment.yml', 'r') as file:
+with open('version.txt', 'r') as file:
     Version=file.read()
     # print(Version)
 with open('kustomize/deployment.yml', 'r') as file:
@@ -16,8 +16,12 @@ for str in splitDepoymentContent:
     if(imageName not in str):
         newDeployment+=str+"\n"
     else:
-        newDeployment+=imageName+Version+"  # Replace with your Docker image and tag\n"
+        print(imageName+Version+"  # Replace with your Docker image and tag\n")
+        newDeployment+=imageName+":"+Version+"  # Replace with your Docker image and tag\n"
 # print(newDeployment)
 
 with open('kustomize/deployment.yml', 'w') as file:
     file.write(newDeployment)
+
+# with open('output.yml', 'w') as file:
+#     file.write(newDeployment)
